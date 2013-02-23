2b4a8f90f6ca84f7e11816a4105c44f75c0114c5
Leonard Law <leon.wlaw@gmail.com>
2013-02-23 17:53:27 -0500

    Initial version of Web UI
    Demos are included

---
+++ web/includes/navigation-menu.js
@@ -0,0 +1,4 @@
+document.write("<li data-role=\"list-divider\">jQuery Mobile</li>");
+document.write("<li><a href=\"./\">Home</a></li>");
+document.write("<li data-role=\"list-divider\">Widget reference</li>");
+document.write("<li data-section=\"Widgets\" data-filtertext=\"table, column toggle tables responsive tables rwd responsive\"><a href=\"widgets/table-column-toggle/\">Table, column toggle <span class=\"ui-li-count\">New</span></a></li>");
