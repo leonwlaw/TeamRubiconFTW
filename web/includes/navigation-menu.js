081fffb502d703e4a4deedeca45a93e01c27c7b6
Leonard Law <leon.wlaw@gmail.com>
2013-02-23 20:33:44 -0500

    Generalized sidebar definitions
    Its easier to read now

--- web/includes/navigation-menu.js
+++ web/includes/navigation-menu.js
@@ -1,5 +1,24 @@
-document.write("<li data-role=\"list-divider\">jQuery Mobile</li>");
-document.write("<li><a href='index.html'>Home</a></li>");
-document.write("<li> <a href=\"login.html\" data-rel=\"dialog\">Sign In</a></li>");
-document.write("<li data-role=\"list-divider\">Widget reference</li>");
-document.write("<li data-section=\"Widgets\" data-filtertext=\"table, column toggle tables responsive tables rwd responsive\"><a href=\"widgets/table-column-toggle/\">Table, column toggle <span class=\"ui-li-count\">New</span></a></li>");
+/*
+To use this function, add what you want to be visible inside the string.
+e.g. to add a link:
+	navmenu_add_entry('<a href="path">Link to page</a>')
+To add a header:
+	navmenu_add_entry('<a href="path">Link to page</a>', true)
+*/
+function navmenu_add_entry(str, is_header) {
+	// Entries are not headers by default
+	if (is_header === undefined)
+		is_header = false
+	
+	var output_str = "<li"
+	if (is_header)
+		output_str += " data-role=\"list-divider\""
+	
+	output_str += '>'+str+'</li>'
+	document.write(output_str)
+}
+
+navmenu_add_entry("Team Rubicon", true);
+navmenu_add_entry("<a href=\"./\">Inventory</a>");
+navmenu_add_entry('<a href="./select_inventory.html">Change Inventory</a>');
+navmenu_add_entry("<a href=\"login.html\" data-rel=\"dialog\">Sign In</a>");
