69a742cdfd1f812ba1e6c16b2954259279d2d609
Mohammed Haque <mh1ok2@gmail.com>
2013-02-23 20:00:51 -0500

    added a login page and updated menu

--- web/includes/navigation-menu.js
+++ web/includes/navigation-menu.js
@@ -1,4 +1,5 @@
 document.write("<li data-role=\"list-divider\">jQuery Mobile</li>");
-document.write("<li><a href=\"./\">Home</a></li>");
+document.write("<li><a href='index.html'>Home</a></li>");
+document.write("<li> <a href=\"login.html\" data-rel=\"dialog\">Sign In</a></li>");
 document.write("<li data-role=\"list-divider\">Widget reference</li>");
 document.write("<li data-section=\"Widgets\" data-filtertext=\"table, column toggle tables responsive tables rwd responsive\"><a href=\"widgets/table-column-toggle/\">Table, column toggle <span class=\"ui-li-count\">New</span></a></li>");
